9ab17b1968a49836c48a5203ea276a69e2f6e8c9
Benjamin Fischer <benjamin.fischer@evolutics.info>
2021-02-06 20:03:25 +0100

    List divergence functions using inspection

--- src/lib/test/test_divergence.py
+++ src/lib/test/test_divergence.py
@@ -1,3 +1,4 @@
+import inspect
 import math
 
 from pytest import mark
@@ -8,71 +9,67 @@ from src.lib import randomness
 
 
 def _cases():
-    return {
-        divergence.euclidean: [
-            (numpy.array([0.2, 0.3, 0.5]), numpy.array([0.3, 0.1, 0.6]), 0.2449489743),
-        ],
-        divergence.hellinger: [
-            (
-                numpy.array([0.2, 0.3, 0.5]),
-                numpy.array([0.3, 0.1, 0.6]),
-                0.1847251674,
-            )
-        ],
-        divergence.jensen_shannon_distance: [
-            (
-                numpy.array([0.2, 0.3, 0.5]),
-                numpy.array([0.3, 0.1, 0.6]),
-                0.182953966,
-            ),
-        ],
-        divergence.k_directed: [
-            (
-                numpy.array([0.2, 0.3, 0.5]),
-                numpy.array([0.3, 0.1, 0.6]),
-                0.02935573227,
-            ),
-        ],
-        divergence.kullback_leibler: [
-            (
-                numpy.array([9 / 25, 12 / 25, 4 / 25]),
-                numpy.array([1 / 3, 1 / 3, 1 / 3]),
-                0.0852996013,
-            ),
-        ],
-        divergence.neyman_chi_square: [
-            (
-                numpy.array([0.2, 0.3, 0.5]),
-                numpy.array([0.3, 0.1, 0.6]),
-                0.45,
-            ),
-        ],
-        divergence.pearson_chi_square: [
-            (
-                numpy.array([0.2, 0.3, 0.5]),
-                numpy.array([0.3, 0.1, 0.6]),
-                0.2033333333,
-            ),
-        ],
-        divergence.total_variation: [
-            (numpy.array([0.2, 0.3, 0.5]), numpy.array([0.3, 0.1, 0.6]), 0.2),
-            (numpy.array([1, 0]), numpy.array([0, 1]), 1),
-        ],
-    }
+    return [
+        (
+            divergence.euclidean,
+            numpy.array([0.2, 0.3, 0.5]),
+            numpy.array([0.3, 0.1, 0.6]),
+            0.2449489743,
+        ),
+        (
+            divergence.hellinger,
+            numpy.array([0.2, 0.3, 0.5]),
+            numpy.array([0.3, 0.1, 0.6]),
+            0.1847251674,
+        ),
+        (
+            divergence.jensen_shannon_distance,
+            numpy.array([0.2, 0.3, 0.5]),
+            numpy.array([0.3, 0.1, 0.6]),
+            0.182953966,
+        ),
+        (
+            divergence.k_directed,
+            numpy.array([0.2, 0.3, 0.5]),
+            numpy.array([0.3, 0.1, 0.6]),
+            0.02935573227,
+        ),
+        (
+            divergence.kullback_leibler,
+            numpy.array([9 / 25, 12 / 25, 4 / 25]),
+            numpy.array([1 / 3, 1 / 3, 1 / 3]),
+            0.0852996013,
+        ),
+        (
+            divergence.neyman_chi_square,
+            numpy.array([0.2, 0.3, 0.5]),
+            numpy.array([0.3, 0.1, 0.6]),
+            0.45,
+        ),
+        (
+            divergence.pearson_chi_square,
+            numpy.array([0.2, 0.3, 0.5]),
+            numpy.array([0.3, 0.1, 0.6]),
+            0.2033333333,
+        ),
+        (
+            divergence.total_variation,
+            numpy.array([0.2, 0.3, 0.5]),
+            numpy.array([0.3, 0.1, 0.6]),
+            0.2,
+        ),
+        (divergence.total_variation, numpy.array([1, 0]), numpy.array([0, 1]), 1),
+    ]
 
 
-def _cases_table():
-    return [(key,) + value for key, values in _cases().items() for value in values]
-
-
-@mark.parametrize("D,p,q,expected", _cases_table())
+@mark.parametrize("D,p,q,expected", _cases())
 def test_single(D, p, q, expected):
     actual = D(p, q)
 
     assert math.isclose(actual, expected)
 
 
-@mark.parametrize("D,p,q,expected", _cases_table())
+@mark.parametrize("D,p,q,expected", _cases())
 def test_broadcast(D, p, q, expected):
     Q = numpy.column_stack([p, q])
 
@@ -82,8 +79,8 @@ def test_broadcast(D, p, q, expected):
     assert math.isclose(actual[1], expected)
 
 
-@mark.parametrize("D", _cases())
-def test_is_nonnegative(D, generator):
+@mark.parametrize("_name,D", inspect.getmembers(divergence, inspect.isfunction))
+def test_is_nonnegative(_name, D, generator):
     for _ in range(20):
         p = randomness.draw_distribution(generator, 5, nonzero_count=5)
         q = randomness.draw_distribution(generator, 5, nonzero_count=5)
@@ -93,8 +90,8 @@ def test_is_nonnegative(D, generator):
         assert value >= 0
 
 
-@mark.parametrize("D", _cases())
-def test_is_zero_if_same(D, generator):
+@mark.parametrize("_name,D", inspect.getmembers(divergence, inspect.isfunction))
+def test_is_zero_if_same(_name, D, generator):
     for _ in range(20):
         p = randomness.draw_distribution(generator, 5, nonzero_count=5)
 
