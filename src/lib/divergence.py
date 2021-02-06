c2c202a28310a835bb3aa769f3560e1830a063aa
Benjamin Fischer <benjamin.fischer@evolutics.info>
2021-02-06 19:58:15 +0100

    Use library function directly for simplicity

--- src/lib/divergence.py
+++ src/lib/divergence.py
@@ -7,42 +7,40 @@ import numpy
 
 
 def euclidean(p, Q):
-    return _column_wise(Q, lambda q: linalg.norm(p - q))
+    return numpy.apply_along_axis(lambda q: linalg.norm(p - q), 0, Q)
 
 
 def hellinger(p, Q):
     factor = 1 / math.sqrt(2)
     sqrt_p = numpy.sqrt(p)
-    return factor * _column_wise(Q, lambda q: linalg.norm(sqrt_p - numpy.sqrt(q)))
+    return factor * numpy.apply_along_axis(
+        lambda q: linalg.norm(sqrt_p - numpy.sqrt(q)), 0, Q
+    )
 
 
 def jensen_shannon_distance(p, Q):
     """Square root of Jensen-Shannon divergence."""
 
-    return _column_wise(Q, lambda q: distance.jensenshannon(p, q))
+    return numpy.apply_along_axis(lambda q: distance.jensenshannon(p, q), 0, Q)
 
 
 def k_directed(p, Q):
     """See: Jianhua Lin. "Divergence Measures Based on the Shannon Entropy". 1991."""
 
-    return _column_wise(Q, lambda q: stats.entropy(p, (p + q) / 2))
+    return numpy.apply_along_axis(lambda q: stats.entropy(p, (p + q) / 2), 0, Q)
 
 
 def kullback_leibler(p, Q):
-    return _column_wise(Q, lambda q: stats.entropy(p, q))
+    return numpy.apply_along_axis(lambda q: stats.entropy(p, q), 0, Q)
 
 
 def neyman_chi_square(p, Q):
-    return _column_wise(Q, lambda q: numpy.sum(numpy.square(p - q) / q))
+    return numpy.apply_along_axis(lambda q: numpy.sum(numpy.square(p - q) / q), 0, Q)
 
 
 def pearson_chi_square(p, Q):
-    return _column_wise(Q, lambda q: numpy.sum(numpy.square(p - q) / p))
+    return numpy.apply_along_axis(lambda q: numpy.sum(numpy.square(p - q) / p), 0, Q)
 
 
 def total_variation(p, Q):
-    return 0.5 * _column_wise(Q, lambda q: linalg.norm(p - q, 1))
-
-
-def _column_wise(matrix, function):
-    return numpy.apply_along_axis(function, axis=0, arr=matrix)
+    return 0.5 * numpy.apply_along_axis(lambda q: linalg.norm(p - q, 1), 0, Q)
