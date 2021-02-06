06e4fb97aead8c992e09e43dbd36c681ec268831
Benjamin Fischer <benjamin.fischer@evolutics.info>
2021-02-06 12:27:38 +0100

    Define sparse approximation case

---
+++ src/lib/approximation/sparse/__init__.py
@@ -0,0 +1,14 @@
+"""
+Minimizes D(b, Ax) for x ∈ Δ^N subject to ‖x‖₀ ≤ K where aₙ, b ∈ Δ^M.
+
+This is a case of sparse approximation.
+
+Δ^L denotes the standard L-simplex, that is, the set of L-dimensional vectors
+with nonnegative entries that sum up to 1 (probability vectors).
+
+We have
+    A ∈ ℝ^{M×N} with each column aₙ ∈ Δ^M,
+    b ∈ Δ^M,
+    D is a divergence, and
+    K ∈ ℕ.
+"""
