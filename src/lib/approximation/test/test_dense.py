37c4a2f41ca376eb498dc5adeeaef15a85a512b5
Benjamin Fischer <benjamin.fischer@evolutics.info>
2021-02-07 16:54:54 +0100

    Test more dense cases automatically

--- src/lib/approximation/test/test_dense.py
+++ src/lib/approximation/test/test_dense.py
@@ -1,12 +1,15 @@
+from pytest import mark
 import numpy
 
 from src.lib.approximation import dense
+from src.lib.test import testing
 
 
-def test_total_variation_exact_case():
+@mark.parametrize("_name,solve", testing.public_functions(dense))
+def test_exact_case(_name, solve):
     A = numpy.array([[3 / 4, 1 / 3], [1 / 4, 2 / 3]])
     b = numpy.array([3 / 5, 2 / 5])
 
-    x = dense.total_variation(A, b)
+    x = solve(A, b)
 
     assert numpy.allclose(x, numpy.array([16 / 25, 9 / 25]))
