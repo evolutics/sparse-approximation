c7f9248b0293b5d37f5bd6b225abfb8f6cc61bcb
Benjamin Fischer <benjamin.fischer@evolutics.info>
2021-02-10 20:09:27 +0100

    Add function to get indices of many biggest entries of array

--- src/lib/sorting.py
+++ src/lib/sorting.py
@@ -1,6 +1,12 @@
 import numpy
 
 
+def argmaxs(values, count):
+    """Indices of `count` biggest entries (order undefined)."""
+
+    return numpy.argpartition(values, kth=-count)[-count:]
+
+
 def argmins(values, count):
     """Indices of `count` smallest entries (order undefined)."""
 
--- src/lib/test/test_sorting.py
+++ src/lib/test/test_sorting.py
@@ -10,6 +10,26 @@ from src.lib import sorting
         (numpy.array([]), 0, numpy.array([])),
         (numpy.array([100]), 0, numpy.array([])),
         (numpy.array([100]), 1, numpy.array([0])),
+        (numpy.array([100, 101]), 1, numpy.array([1])),
+        (numpy.array([101, 100]), 1, numpy.array([0])),
+        (numpy.array([100, 100]), 2, numpy.array([0, 1])),
+        (numpy.array([101, 103, 100, 102]), 2, numpy.array([1, 3])),
+        (numpy.array([101, 100, 100, 101]), 2, numpy.array([0, 3])),
+        (numpy.array([100, 101, 102, 100, 102]), 3, numpy.array([1, 2, 4])),
+    ],
+)
+def test_argmaxs(values, count, expected_indices):
+    actual_indices = sorting.argmaxs(values, count)
+
+    assert all(numpy.sort(actual_indices) == expected_indices)
+
+
+@mark.parametrize(
+    "values,count,expected_indices",
+    [
+        (numpy.array([]), 0, numpy.array([])),
+        (numpy.array([100]), 0, numpy.array([])),
+        (numpy.array([100]), 1, numpy.array([0])),
         (numpy.array([100, 101]), 1, numpy.array([0])),
         (numpy.array([101, 100]), 1, numpy.array([1])),
         (numpy.array([100, 100]), 2, numpy.array([0, 1])),
