52b00b721b91c95da3674a454e612685c72dfd4a
Benjamin Fischer <benjamin.fischer@evolutics.info>
2021-02-27 17:04:06 +0100

    Drop assumption of probability simplex for generality

--- src/lib/approximation/sparse/__init__.py
+++ src/lib/approximation/sparse/__init__.py
@@ -1,14 +1,13 @@
 """
-Minimizes D(b, Ax) for x ∈ ℝ₊^N subject to ‖x‖₀ ≤ K where aₙ, b ∈ Δ^M.
+Minimizes D(b, Ax) for x ∈ ℝ₊^N subject to ‖x‖₀ ≤ K where aₙ, b ∈ ℝ₊^M.
 
 This is a case of sparse approximation.
 
-Δ^L denotes the standard L-simplex, that is, the set of L-dimensional vectors
-with nonnegative entries that sum up to 1 (probability vectors).
+ℝ₊ denotes the nonnegative real numbers.
 
 We have
-    A ∈ ℝ^{M×N} with each column aₙ ∈ Δ^M,
-    b ∈ Δ^M,
+    A ∈ ℝ₊^{M×N},
+    b ∈ ℝ₊^M,
     D is a divergence, and
     1 ≤ K < N.
 """
