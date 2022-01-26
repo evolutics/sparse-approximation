6474863e1917c8499a4c832b84188f7ea7109b96
Benjamin Fischer <benjamin.fischer@evolutics.info>
2022-01-26 22:46:54 +0100

    Allow constant step size as in original Warm-KL

--- src/lib/approximation/sparse/multi_warm_js_subspace_pursuit.py
+++ src/lib/approximation/sparse/multi_warm_js_subspace_pursuit.py
@@ -23,7 +23,12 @@ def solve(A, b, D, K, *, solve_dense, lambdas, I, L):
             return x
 
     for lambda_ in lambdas:
-        eta_i = lambda i, lambda_=lambda_: 1 / (lambda_ * i + 1)
+        eta_i = (
+            lambda i, lambda_=lambda_: -lambda_
+            if lambda_ < 0
+            else 1 / (lambda_ * i + 1)
+        )
+
         xs_ = _iterate(A=A, b=b, eta_i=eta_i)
 
         for x in itertools.islice((x for i, x in enumerate(xs_, 1) if i in I), len(I)):
