4bfe057da960993a5266320ba8ceba78d54bc5c3
Benjamin Fischer <benjamin.fischer@evolutics.info>
2022-02-06 22:56:59 +0100

    Rename index variables

--- src/lib/approximation/sparse/frank_wolfe.py
+++ src/lib/approximation/sparse/frank_wolfe.py
@@ -8,17 +8,17 @@ def solve(A, b, D, K, *, solve_dense, normalize, is_step_size_adaptive):
 
     for i in range(K):
         potentials = D(normalize(r), A[:, ~S])
-        n = numpy.flatnonzero(~S)[numpy.argmin(potentials)]
-        S[n] = True
+        index = numpy.flatnonzero(~S)[numpy.argmin(potentials)]
+        S[index] = True
 
         if i == 0:
-            y = A[:, n]
+            y = A[:, index]
         else:
             if is_step_size_adaptive:
-                step_size = solve_dense(numpy.column_stack([y, A[:, n]]), b)[1]
+                step_size = solve_dense(numpy.column_stack([y, A[:, index]]), b)[1]
             else:
                 step_size = 2 / (i + 2)
-            y = (1 - step_size) * y + step_size * A[:, n]
+            y = (1 - step_size) * y + step_size * A[:, index]
 
         r = b - y
 
--- src/lib/approximation/sparse/orthogonal_matching_pursuit.py
+++ src/lib/approximation/sparse/orthogonal_matching_pursuit.py
@@ -8,8 +8,8 @@ def solve(A, b, D, K, *, solve_dense, normalize):
 
     while numpy.count_nonzero(S) < K:
         potentials = D(normalize(r), A[:, ~S])
-        n = numpy.flatnonzero(~S)[numpy.argmin(potentials)]
-        S[n] = True
+        index = numpy.flatnonzero(~S)[numpy.argmin(potentials)]
+        S[index] = True
 
         x = numpy.zeros(N)
         x[S] = solve_dense(A[:, S], b)
