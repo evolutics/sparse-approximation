90dc910c304496728f177d0a045c67b410ea8c53
Benjamin Fischer <benjamin.fischer@evolutics.info>
2022-04-17 11:51:47 +0200

    Simplify folder handling

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,10 +6,9 @@ set -o pipefail
 
 main() {
   local -r script_folder="$(dirname "$(readlink --canonicalize "$0")")"
-  local -r project_folder="$(dirname "${script_folder}")"
-  cd "${project_folder}"
+  cd "$(dirname "${script_folder}")"
 
-  docker run --entrypoint sh --rm --volume "$(pwd)":/workdir \
+  docker run --entrypoint sh --rm --volume "${PWD}":/workdir \
     evolutics/travel-kit:0.8.0 -c \
     'git ls-files -z | xargs -0 travel-kit check --skip Pylint --'
 
